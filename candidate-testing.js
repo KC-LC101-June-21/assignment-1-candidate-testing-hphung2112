04c4a70272e570c387e591888e1367f262151424
Hanh Phung <38336910+hphung2112@users.noreply.github.com>
2021-06-11 02:07:51 +0000

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,46 +1,52 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/ 2* 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 
 // TODO 1.1a: Define candidateName // 
 
 let candidateName = ' ';
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
+let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
-let candidateAnswer = ' ';
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let candidateAnswer = [];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question('Please enter your name: ')
+  candidateName = input.question('Please enter your name: ');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-console.log(candidateAnswer);
+  //candidateAnswer = input.question(question);
+
+  for (i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  }
+  console.log(candidateAnswers[i]);
+  //candidateAnswer = input.question(question);
+  //console.log(candidateAnswer);
 }
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz(candidateAnswer) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if ('candidateAnswer' === 'correctAnswer'){
-  console.log("Candidate's answer is correct");
-  } else {
-    console.log("Candidate's answer is incorrect");
+  //if (candidateAnswer === correctAnswer) {
+   // console.log("Candidate's answer is correct");
+ // } else {
+   // console.log("Candidate's answer is incorrect");
+  for (i = 0; i < questions.length; i++) {
+    console.log(`The candidate answer is ${candidateAnswers} and the correct answer is ${correctAnswers}`);
   }
-}
+  }
 
-  let grade;
-  
+let grade;
 
-  return grade;
-}
+
+return grade;
 
 function runProgram() {
   askForName();
